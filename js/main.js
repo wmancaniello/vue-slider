72cb528439c882a5729ddec2c080af36c04e3a84
wmancaniello <wmancaniello@gmail.com>
2024-04-18 17:09:56 +0200

    set carousel and thumbnails

--- js/main.js
+++ js/main.js
@@ -1,39 +1,6 @@
-const images = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
-
-// PUNTO DI PARTENZA
-
-console.log(Vue);
-
 const { createApp } = Vue;
 
 createApp({
-  // Inserire dati e logica dell'app
   data() {
     return {
       images: [
@@ -63,9 +30,11 @@ createApp({
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
       ],
+      currentIndex: 0,
     };
   },
   methods: {
-    // Metto tutte le funzioni che posso invocare nell'app
+    // Funzioni
+
   },
-}).mount("#app");
+}).mount("#my-carousel");
